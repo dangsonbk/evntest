d995545084d32d68a7aa0d8bd2a94ba9f231c112
dangsonbk <dangson302@gmail.com>
2021-04-22 11:30:41 +0700

    Fix form

--- evntest/urls.py
+++ evntest/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 import django.contrib.auth.views as auth_views
 
 urlpatterns = [
-    path('accounts/login/', auth_views.LoginView.as_view()),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view()),
     path('admin/', admin.site.urls),
     path('', include('quiz.urls'))
