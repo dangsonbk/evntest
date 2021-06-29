a97db63faff73dc7e79e40e5c9a236e5dc6d19f0
letrii <le.duc.tri.niit@gmail.com>
2021-06-29 11:50:22 +0700

    fix question layout and display image

--- evntest/urls.py
+++ evntest/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 import django.contrib.auth.views as auth_views
 from quiz.views import CustomLoginView
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -24,4 +26,4 @@ urlpatterns = [
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
     path('', include('quiz.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
